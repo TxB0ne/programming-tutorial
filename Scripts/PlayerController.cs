995a5a7e9a15488ff3df034558169cba617969a8
TxB0ne <118011462+TxB0ne@users.noreply.github.com>
2023-02-02 21:36:41 -0500

    Add files via upload

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -4,7 +4,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    
+    public float moveSpeed;
+    private Rigidbody rigidBody;
+    public float jumpForce;
+
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
     void Start()
     {
         
@@ -13,6 +21,36 @@ public class PlayerController : MonoBehaviour
     
     void Update()
     {
+        Move();
+        Jump();
+    }
+
+    void Move()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+        Vector3 dir = new Vector3(xInput, 0, zInput) * moveSpeed;
+        dir.y = rigidBody.velocity.y;
+        rigidBody.velocity = dir;
+
+        Vector3 facingDir = new Vector3(xInput, 0, zInput);
+        if (facingDir.magnitude > 0)
+        {
+            transform.forward = facingDir;
+        }
         
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            checkJumpForce();
+        }
+    }
+
+    void checkJumpForce()
+    {
+        rigidBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
     }
 }
